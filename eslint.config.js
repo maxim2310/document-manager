8fedd4a621f6a2900a2d3524f2f6a35a56ae7a17
Maxim Kantsura <maxim@smoothr.de>
2025-02-22 22:48:41 +0200

    init project

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const angular = require("angular-eslint");
+const tseslint = require("typescript-eslint");
+const eslint = require("@eslint/js");
+
+
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
