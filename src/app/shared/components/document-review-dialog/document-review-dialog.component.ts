fc699bfe36a0a8d4bb3d61d8468f742ea55b3a61
Maxim Kantsura <maxim@smoothr.de>
2025-02-25 04:05:24 +0200

    added review logic

---
+++ src/app/shared/components/document-review-dialog/document-review-dialog.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  Inject,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import PSPDFKit  from 'pspdfkit';
+
+const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
+
+@Component({
+  imports: [MatButtonModule],
+  selector: 'app-document-review-dialog',
+  templateUrl: './document-review-dialog.component.html',
+  styleUrls: ['./document-review-dialog.component.scss'],
+})
+export class DocumentReviewDialogComponent implements AfterViewInit {
+  @ViewChild('pspdfContainer', { static: false }) containerRef!: ElementRef;
+  instance!: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { documentUrl: string },
+    private dialogRef: MatDialogRef<DocumentReviewDialogComponent>
+  ) {}
+
+  async ngAfterViewInit(): Promise<void> {    
+    try {
+      this.instance = await PSPDFKit.load({
+        container: this.containerRef.nativeElement,
+        document: this.data.documentUrl,
+        baseUrl: baseUrl,
+      });
+    } catch (error) {
+      console.error('Error loading PSPDFKit:', error);
+    }
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
